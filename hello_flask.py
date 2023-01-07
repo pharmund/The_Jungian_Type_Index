5b060bbae970829ba43457be5fef35a5a5db8127
pharmund <pharmund@student.21-school.ru>
2023-01-07 23:38:29 +0300

    страница 256

--- hello_flask.py
+++ hello_flask.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, render_template
+from text_analyzer import AspectDict
+import aspect_words
 
 app = Flask(__name__)
 
@@ -6,14 +8,39 @@ app = Flask(__name__)
 def hello() -> str:
     return 'Hello from Flask'
 
-# @app.route('/')
-# def my_form():
-#     return render_template('my-form.html')
+@app.route('/search4')
+def do_search():
 
-# @app.route('/', methods=['POST'])
-# def my_form_post():
-#     text = request.form['text']
-#     processed_text = text.upper()
-#     return processed_text
+    text = input("Введите текст: ")
+    text = text.lower()
+    text = text.rstrip()
+
+    SE = AspectDict(aspect_words.se)
+    SI = AspectDict(aspect_words.si)
+    TE = AspectDict(aspect_words.te)
+    TI = AspectDict(aspect_words.ti)
+    FE = AspectDict(aspect_words.fe)
+    FI = AspectDict(aspect_words.fi)
+    NE = AspectDict(aspect_words.ne)
+    NI = AspectDict(aspect_words.ni)
+
+    """ Создадим финальный словарь """
+
+    final_dict = {}
+    final_dict['SE'] = SE.analyze(text)
+    final_dict['SI'] = SI.analyze(text)
+    final_dict['TE'] = TE.analyze(text)
+    final_dict['TI'] = TI.analyze(text)
+    final_dict['FE'] = FE.analyze(text)
+    final_dict['FI'] = FI.analyze(text)
+    final_dict['NE'] = NE.analyze(text)
+    final_dict['NI'] = NI.analyze(text)
+
+    return final_dict
+
+@app.route('/entry') 
+def entry_page() -> 'html': 
+ return render_template('entry.html', 
+ the_title='Welcome to search4letters on the web!')
 
 app.run()

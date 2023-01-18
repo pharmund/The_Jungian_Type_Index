e39c5f6396e931f16f122324b6487f9e8e0c5ed4
pharmund <sashkinapochta@inbox.ru>
2023-01-18 19:06:50 +0300

    Сделал таблицу

--- hello.py
+++ hello.py
@@ -1,16 +1,12 @@
 from flask import Flask, request, render_template, Markup
 from text_analyzer import AspectDict
 import aspect_words
-# import pandas as pd
-# import numpy as np
-
+from itertools import chain
+from itertools import zip_longest
 
 
 application = Flask(__name__)
 application.config['JSON_AS_ASCII'] = False
-
-
-
 
 
 @application.route('/search4', methods=['POST'])
@@ -18,10 +14,6 @@ def do_search() -> dict:
 
     text = request.form['phrase']
     title = 'Here are your results:'
-
-    # text = input("Введите текст: ")
-    # text = text.lower()
-    # text = text.rstrip()
 
     SE = AspectDict(aspect_words.se)
     SI = AspectDict(aspect_words.si)
@@ -59,26 +51,74 @@ def do_search() -> dict:
     results = first_dict
     results2 = final_dict
 
-    # df = pd.DataFrame.from_dict(results2)
+    sorted_SE = sorted(SE.analyze(text).items(), key=lambda x: x[1], reverse=True)
+    tuple_SE = (list(first_dict)[0], first_dict['SE'])
+    sorted_SE.insert(0, tuple_SE)
+    sorted_SE = list(map(list, sorted_SE))
+
+    sorted_SI = sorted(SI.analyze(text).items(), key=lambda x: x[1], reverse=True)
+    tuple_SI = (list(first_dict)[1], first_dict['SI'])
+    sorted_SI.insert(0, tuple_SI)
+    sorted_SI = list(map(list, sorted_SI))
+
+    sorted_TE = sorted(TE.analyze(text).items(), key=lambda x: x[1], reverse=True)
+    tuple_TE = (list(first_dict)[2], first_dict['TE'])
+    sorted_TE.insert(0, tuple_TE)
+    sorted_TE = list(map(list, sorted_TE))
+
+    sorted_TI = sorted(TI.analyze(text).items(), key=lambda x: x[1], reverse=True)
+    tuple_TI = (list(first_dict)[3], first_dict['TI'])
+    sorted_TI.insert(0, tuple_TI)
+    sorted_TI = list(map(list, sorted_TI))
+
+    sorted_FE = sorted(FE.analyze(text).items(), key=lambda x: x[1], reverse=True)
+    tuple_FE = (list(first_dict)[4], first_dict['FE'])
+    sorted_FE.insert(0, tuple_FE)
+    sorted_FE = list(map(list, sorted_FE))
+
+    sorted_FI = sorted(FI.analyze(text).items(), key=lambda x: x[1], reverse=True)
+    tuple_FI = (list(first_dict)[5], first_dict['FI'])
+    sorted_FI.insert(0, tuple_FI)
+    sorted_FI = list(map(list, sorted_FI))
+
+    sorted_NE = sorted(NE.analyze(text).items(), key=lambda x: x[1], reverse=True)
+    tuple_NE = (list(first_dict)[6], first_dict['NE'])
+    sorted_NE.insert(0, tuple_NE)
+    sorted_NE = list(map(list, sorted_NE))
+
+    sorted_NI = sorted(NI.analyze(text).items(), key=lambda x: x[1], reverse=True)
+    tuple_NI = (list(first_dict)[7], first_dict['NI'])
+    sorted_NI.insert(0, tuple_NI)
+    sorted_NI = list(map(list, sorted_NI))
+
 
     bar_labels = list(results)
-    # print (bar_labels)
     bar_values = list(results.values())
-    return render_template('results.html', title='', max=max(bar_values), labels=bar_labels, values=bar_values,
-                            the_results2=results2,
-                            )
 
-    return render_template('results.html',
-                           the_phrase=text,
-                           the_title=title,
-                           the_results=results,
-                           the_results2=results2,)
+
+    result_list = list(zip_longest(sorted_SE, sorted_SI, sorted_TE, sorted_TI, sorted_FE, sorted_FI, sorted_NE, sorted_NI, fillvalue=['0',0]))
+    result_list = list(map(list, result_list)) # лист листов
+
+    new_result_list = []
+    for element in result_list:
+        element = list(chain.from_iterable(element))
+        new_result_list.append(element )
+
+
+ 
+
+
+    return render_template('results.html', title='', max=max(bar_values), labels=bar_labels, values=bar_values,
+                            # # the_results8=sorted_NI,
+                            # the_results8=new_result_list,
+                            table = new_result_list,
+                            )
 
 @application.route('/')
 @application.route('/entry') 
 def entry_page() -> 'html': 
     return render_template('entry.html',
-    the_title='Welcome to search4letters on the web!')
+    the_title='Welcome to search4words on the web!')
 
 
 if __name__ == '__main__':
